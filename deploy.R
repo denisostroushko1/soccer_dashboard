91c2ce2eaa2897ec5ec325ab89862d84793d585a
kexite <denisostroushko1@gmail.com>
2023-04-10 22:17:06 -0500

    finally

--- deploy.R
+++ deploy.R
@@ -1,8 +1,5 @@
 
 # https://nrennie.rbind.io/blog/2022-10-05-automatically-deploying-a-shiny-app-for-browsing-rstats-tweets-with-github-actions/
-rsconnect::setAccountInfo(name='kexite',
-			  token='6CCB37F9420F50D615E3A222E8C297A1',
-			  secret='61LtkxrGX+wr4y/0KyyL5KrB96quAxv0wAnXqHW1')
     # Authenticate
     setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                    token = Sys.getenv("SHINY_TOKEN"),
